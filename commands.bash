d618db9fe74ee1f63255592ee96e3baf5c4196a7
seancarey <seancarey@careys.io>
2022-06-01 20:41:23 -0400

    chagne

--- commands.bash
+++ commands.bash
@@ -4,3 +4,4 @@ prefect create project jaffle_shop
 prefect register --project jaffle_shop -p flows/
 # adjust the path to where you cloned the repository e.g. -p ~/repos/flow-of-flows/
 prefect agent local start --label dev --no-hostname-label -p ~/projects/flow-of-flows/
+prefect run --id 54d6edfb-489a-4653-be5d-8ae5327e4264 --watch
